34e4d4508ac9a71355430bcfb699f1bde5781197
cearamullins <cearamullins@gmail.com>
2020-09-16 22:42:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,4 @@
 
 window.onload = function () {
     console.log("Loading the page");
-    //document.body.innerHTML = testing();
-}
-
-function testing() {
-    return `
-        <div id=header>
-        </div>
-        <div id=workspace>
-        </div>
-        <div id=footer>
-        </div>
-    `
 }
