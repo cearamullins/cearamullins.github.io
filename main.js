81dbe8430d1d3e4a4a6a5db303ce2b533ff5ac34
cearamullins <cearamullins@gmail.com>
2020-09-16 22:32:54 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// this is the main js file
+
+window.onload = function () {
+    console.log("Loading the page");
+    document.body.innerHTML = testing();
+}
+
+function testing() {
+    return `
+        <div id=header>
+        </div>
+        <div id=workspace>
+        </div>
+        <div id=footer>
+        </div>
+    `
+}
